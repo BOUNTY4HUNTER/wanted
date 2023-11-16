f35590f54a8ca0833ec9285a61900a16cf9340a0
DESKTOP-OD2NKAS\sgbee <sg.been2017@gmail.com>
2023-11-17 03:01:17 +0900

    feat: 로그인 input 입력

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,25 @@
 'use client';
 
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLogin = () => {
+        // Perform login logic with username and password
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center text-xl bg-primary-1">
             <div className="font-extrabold text-5xl mb-20 mt-32 text-primary-5">
@@ -13,18 +30,25 @@ export default function Login() {
                     className="flex border border-solid border-black rounded-lg p-2 h-9 mb-3"
                     type="text"
                     placeholder="아이디를 입력해 주세요"
+                    value={username}
+                    onChange={handleUsernameChange}
                 />
                 <input
                     className="flex border border-solid border-black rounded-lg p-2 h-9 mb-8"
                     type="password"
                     placeholder="비밀번호를 입력해 주세요"
+                    value={password}
+                    onChange={handlePasswordChange}
                 />
             </div>
-            <button className="flex justify-center items-center rounded-lg w-5/6 h-9 bg-primary-5 text-white mx-auto">
+            <button
+                className="flex justify-center items-center rounded-lg w-5/6 h-10 bg-primary-5 text-white mx-auto"
+                onClick={handleLogin}
+            >
                 로그인
             </button>
             <div className="mt-7 underline">회원가입</div>
-            <div className="mt-2 mb-52 underline">아이디/비밀번호 찾기</div>
+            <div className="mt-2 mb-64 underline">아이디/비밀번호 찾기</div>
         </div>
     );
 }
